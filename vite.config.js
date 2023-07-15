b12b4a4f1571cf26a2a31acc12325d66d09b488f
ymj <3495314473@qq.com>
2023-07-15 17:13:06 +0800

    elementPlus 主题色更换

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,19 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({importStyle: "sass"})],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
   }
 })
